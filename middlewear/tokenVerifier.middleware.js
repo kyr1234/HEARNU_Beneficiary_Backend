60d7213445b9828ba589d55490224ad118537fc9
Yug Khokhar <83321546+kyr1234@users.noreply.github.com>
2023-03-07 12:31:10 +0530

    Remove Authenticator

--- middlewear/tokenVerifier.middleware.js
+++ middlewear/tokenVerifier.middleware.js
@@ -3,35 +3,8 @@ import UserModel from '../models/user/User.schema.js'
 import jwt from 'jsonwebtoken'
 
 
-/* export async function isAuthenticated(req, res, next) {
-  try {
-    const Cookie = req.headers
-    console.log(Cookie)
-    if (Cookie) {
-      const token = Cookie.replace('token=', '')
 
-      if (!token) {
-        return apiResponse.errorResponse(
-          res,
-          'Token Not Found Please Login First',
-        )
-      }
-
-      const decodeddata = jwt.verify(
-        token,
-        process.env.JSON_WEB_TOKEN_SECRET_KEY,
-      )
-
-      req.user_info = await UserModel.findById(decodeddata.id)
-
-      next()
-    } else {
-      return apiResponse.errorResponse(res, 'Token Not Valid')
-    }
-  } catch (e) {
-    return apiResponse.errorResponse(res, e.message)
-  }
-} */export async function isAuthenticated(req, res, next) {
+export async function isAuthenticated(req, res, next) {
   try {
     const Bearer = req.header('Authorization')
     console.log(Bearer)
